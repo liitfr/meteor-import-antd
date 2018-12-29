0dd7c4b4699b15825677312a23e6c42cdc3bdf16
Mathias Hoffmann <hoffmann.mathias@gmail.com>
2018-12-29 02:44:47 +0100

    fix: export directly at root

--- rollup.config.js
+++ rollup.config.js
@@ -3,8 +3,10 @@ const globby = require('globby');
 const configs = globby.sync('src/*.js').map(inputFile => ({
   input: inputFile,
   output: {
-    file: inputFile.replace('src', 'dist'),
+    file: inputFile.replace('src/', ''),
     format: 'cjs',
+    interop: false,
+    compact: true,
   }
 }));
 
